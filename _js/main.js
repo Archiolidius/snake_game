2bb653c6b25aed687a51c406a51147137d130166
Archiolidius <Archiolidius@gmail.com>
2012-12-16 00:04:10 +0200

    Рисуется змейка=)

--- _js/main.js
+++ _js/main.js
@@ -1,7 +1,43 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Archi_000
- * Date: 15.12.12
- * Time: 22:52
- * To change this template use File | Settings | File Templates.
- */
+$(document).ready(function () {
+    var canvas = $('#canvas_snake')[0];
+    var ctx = canvas.getContext('2d');
+    var cur_x; //текущие координаты
+    var cur_y;
+    var length; //текущая длина змейки
+    var h = $('#canvas_snake').height();
+    var w = $('#canvas_snake').width();
+    var direction;
+    var snake_array;
+    var cell_size=10;
+
+
+    function init() {
+        snake();
+        //target();
+        draw();
+    }
+    init();
+
+    function snake() {
+        length = 5;
+        var start_y = 10; // начальное положение у
+        snake_array=[],[];
+        for (i = 0; i <= length; i++) {
+            snake_array.push({x:i*cell_size, y:start_y*cell_size});
+        }
+    }
+
+    function draw(){
+        for(i=0;i<=length;i++){
+            ctx.fillRect(snake_array[i].x,snake_array[i].y,cell_size,cell_size);
+            ctx.strokeStyle = "white";
+            ctx.strokeRect(snake_array[i].x,snake_array[i].y,cell_size,cell_size);
+        }
+    }
+
+    function control(){
+
+    }
+
+
+})
