af34f99f91b0566c77460662183a6d287bb893a7
Archiolidius <Archiolidius@gmail.com>
2012-12-16 17:30:26 +0200

    управляемая змейка, обработка выхода за поле, рисование цели

--- _js/main.js
+++ _js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
         var canvas = $('#canvas_snake')[0];
         var ctx = canvas.getContext('2d');
         var cur_x; //текущие координаты
@@ -9,15 +10,19 @@ $(document).ready(function () {
         var direction;
         var snake_array;
         var cell_size = 10;
+        var speed = 100;
+        var target_x;
+        var target_y;
 
         function init() {
             direction = 'rigth';
+            create_target();
             snake();
-            //target();
-            draw_snake();
-            //control()
-            if (typeof game_loop != "undefined") clearInterval(game_loop);
-            game_loop = setInterval(draw_snake(), 60);
+            setInterval(function () {
+                draw_snake();
+            }, speed);
+
+
         }
 
         init();
@@ -37,52 +42,77 @@ $(document).ready(function () {
             ctx.fillRect(0, 0, w, h);
             ctx.strokeStyle = "black";
             ctx.strokeRect(0, 0, w, h);
-            var x_x = snake_array[0].x;
-            var y_y = snake_array[0].y;
+            cur_x = snake_array[0].x;
+            cur_y = snake_array[0].y;
             for (var i = 0; i < length; i++) {
                 ctx.fillRect(snake_array[i].x, snake_array[i].y, cell_size, cell_size);
                 ctx.strokeStyle = "black";
                 ctx.strokeRect(snake_array[i].x, snake_array[i].y, cell_size, cell_size);
             }
             if (direction == 'rigth') {
-                x_x = x_x + 10;
+                cur_x += 10;
             }
             if (direction == 'left') {
-                x_x = x_x - 10;
+                cur_x -= 10;
             }
             if (direction == 'up') {
-                y_y = y_y - 10;
+                cur_y -= 10;
             }
             if (direction == 'down') {
-                y_y = y_y + 10;
+                cur_y += 10;
             }
             var tail = snake_array.pop(); //pops out the last cell
-            tail.x = x_x;
-            tail.y = y_y;
+            tail.x = cur_x;
+            tail.y = cur_y;
             snake_array.unshift(tail);
             for (var i = 0; i < length; i++) {
+
                 ctx.fillRect(snake_array[i].x * cell_size, snake_array[i].y * cell_size, cell_size, cell_size);
                 ctx.strokeStyle = "black";
                 ctx.strokeRect(snake_array[i].x * cell_size, snake_array[i].y * cell_size, cell_size, cell_size);
+            }
+            draw_target(target_x, target_y);
+            exceptions()
+        }
+
+        function create_target() {
+            var max = h / 10;
+            var min = 0;
+            var rand = Math.round(Math.random() * 10);
+            target_x = rand;
+            target_y = rand;
+            draw_target(target_x, target_y);
+        }
+
+        function draw_target(x, y) {
+            ctx.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(x * cell_size, y * cell_size, cell_size, cell_size);
+        }
+
+        function exceptions() {
+            if (cur_x < 0 || cur_y < 0 || cur_x > w || cur_y > h) {
+                speed = speed + 100000;
+                init();
             }
         }
 
         $(document).bind('keypress', function (e) {
             if (e.keyCode == 119) {
-                direction = 'up';
-                draw_snake();
+                if (direction != 'down')
+                    direction = 'up';
             }
             if (e.keyCode == 100) {
-                direction = 'rigth';
-                draw_snake();
+                if (direction != 'left')
+                    direction = 'rigth';
             }
             if (e.keyCode == 115) {
-                direction = 'down';
-                draw_snake();
+                if (direction != 'up')
+                    direction = 'down';
             }
             if (e.keyCode == 97) {
-                direction = 'left';
-                draw_snake();
+                if (direction != 'rigth')
+                    direction = 'left';
             }
         })
     }
